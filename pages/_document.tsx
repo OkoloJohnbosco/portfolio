b7f0eb51d0cd23033563cbc1aaf293daa0bbc920
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-09-17 01:59:51 +0100

    found the perfect font for me

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <link
+        href="https://fonts.googleapis.com/css2?family=PT+Mono&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital@1&display=swap"
+        rel="stylesheet"
+      />
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Fira+Mono&display=swap"
+        rel="stylesheet"
+      ></link>
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap"
+        rel="stylesheet"
+      ></link>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
