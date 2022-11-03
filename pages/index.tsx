6e914e602893664f217c36ff2c1cbf920212a738
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-11-03 23:59:35 +0100

    adedd header

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Stack>
+        <Navbar />
         <Header />
         <Experience />
         <Projects />
