3501f45e36fb45a6599f0ccdd839d86fd09cb597
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-11-03 23:59:17 +0100

    removed header

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Stack>
-        <Navbar />
         <Header />
         <Experience />
         <Projects />
